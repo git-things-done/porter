bd65c5b27498aa86491f996c960a600b83af6b76
mxcl <mxcl@me.com>
2021-11-20 12:37:10 -0500

    autoclose

---
+++ src/closer.ts
@@ -0,0 +1,18 @@
+import { warning } from '@actions/core'
+import { marked } from 'marked'
+
+export function shouldBeClosed(input: string): boolean {
+  if (!input) {
+    warning("suspiciously empty issue body")
+  }
+
+  let canBeClosed = true
+  marked.use({ walkTokens: (token) => {
+    if (token.type === "list_item" && !token.checked)
+      canBeClosed = false
+  }})
+
+  marked.parse(input)
+
+  return canBeClosed
+}
